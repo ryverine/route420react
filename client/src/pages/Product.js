d1077b6dd1cf1b578ae634be7295c9990ea59449
Isabella Merendino <isabellamerendino@Isabellas-MacBook-Pro.local>
2019-08-22 13:01:26 -0400

    updated store pg jumbotron, added jumbotron to store descrip; added spacing

--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -71,7 +71,7 @@ class Saved extends Component {
 
                     <Jumbotron2>
                         <div>   
-                        <h3 id="productName">{this.state.name}</h3>
+                        <h3 id="underline">{this.state.name}</h3>
                        
                         <div>
                            From: {this.state.brand}
