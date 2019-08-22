b0013036349f73a863ec774c65d735cd038d21ec
Ryan C <48160382+ryverine@users.noreply.github.com>
2019-08-21 22:05:47 -0400

    trying to fix product search
    Original textbox and search button:
    
    <Input
                    value={this.state.searchTerms}
                    onChange={this.handleInputChange}
                    name="searchTerms"
                    placeholder="Search for a Product!"
                  />
    
                  <FormBtn
                    disabled={!(this.state.searchTerms)}
                    onClick={this.doProductSearch}
                  >
                    Find Product
                  </FormBtn>

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -22,110 +22,6 @@ class Search extends Component {
     });
   };
 
-  saveBook = (book) => {
-  /*
-    API.saveBook(book)
-      .then(res => {
-        // removed the saved book from the results list.
-
-        // var myFish = ['angel', 'clown', 'drum', 'mandarin', 'sturgeon'];
-        // var removed = myFish.splice(3, 1);
-        // removed is ["mandarin"]
-        // myFish is ["angel", "clown", "drum", "sturgeon"]
-
-        // find index of book
-        var bookIndex = -1;
-        for (var i = 0; i < this.state.results.length; i++)
-        {
-            if(this.state.results[i].link === book.link)
-            {
-                bookIndex = i;
-                break;
-            }
-        }
-
-        if(bookIndex > -1)
-        {
-            var tmpResults = this.state.results;
-            var removedBook = tmpResults.splice(bookIndex,1);
-            this.setState({results: tmpResults});
-        }
-
-      })
-      .catch(err => console.log(err));
-    */};
-
-  doGoogleBooksSearch = event => {/*
-    event.preventDefault();
-    var searchText = this.state.title.trim();
-    API.googleBooksSearch(searchText).then(res => 
-      {
-        console.log(res.data.items);
-        this.setState({ books: res.data, title: "", author: "", synopsis: "" });
-
-        var resDataItems = res.data.items;
-        var searchResults = [];
-
-        if(resDataItems.length > 0)
-        {
-          for(var i = 0; i < resDataItems.length; i++)
-          {
-            var tmpAuthor = "";
-            var tmpImage = "";
-           
-            if (resDataItems[i].volumeInfo.authors)
-            {
-              tmpAuthor = resDataItems[i].volumeInfo.authors[0];
-            }
-            else
-            {
-              tmpAuthor = "UNKNOWN";
-            }
-
-            if (resDataItems[i].volumeInfo.imageLinks)
-            {
-              tmpImage = resDataItems[i].volumeInfo.imageLinks.thumbnail;
-            }
-            else
-            {
-              tmpImage = "no_thumbnail.png";
-            }
-
-            var tmpBook = {
-              author: tmpAuthor,
-              title: resDataItems[i].volumeInfo.title,
-              description: resDataItems[i].volumeInfo.description,
-              link: resDataItems[i].volumeInfo.infoLink,
-              image: tmpImage
-            };
-
-            searchResults.push(tmpBook);
-
-          }
-        }
-        else
-        {
-          var noResult = {
-            author: "",
-            title: "No results found.",
-            description: "",
-            link: "",
-            image: ""
-          };
-
-          searchResults.push(noResult);
-        }
-
-        this.setState({ 
-          books: res.data, 
-          title: "", 
-          author: "", 
-          synopsis: "",
-          results: searchResults
-         });
-
-      });
-    */};
 
   doProductSearch = event => {
     event.preventDefault();
@@ -163,34 +59,7 @@ class Search extends Component {
           filter: "",
           results: res.data
         });
-
-        /*
-        // removed the saved book from the results list.
-
-        // var myFish = ['angel', 'clown', 'drum', 'mandarin', 'sturgeon'];
-        // var removed = myFish.splice(3, 1);
-        // removed is ["mandarin"]
-        // myFish is ["angel", "clown", "drum", "sturgeon"]
-
-        // find index of book
-        var bookIndex = -1;
-        for (var i = 0; i < this.state.results.length; i++)
-        {
-            if(this.state.results[i].link === book.link)
-            {
-                bookIndex = i;
-                break;
-            }
-        }
-
-        if(bookIndex > -1)
-        {
-            var tmpResults = this.state.results;
-            var removedBook = tmpResults.splice(bookIndex,1);
-            this.setState({results: tmpResults});
-        }*/
-      })
-      .catch(err => console.log(err));
+      }).catch(err => console.log(err));
 
   }
 
@@ -222,9 +91,12 @@ class Search extends Component {
                   <div className="form">
                     <form className="form-inline mr-auto mb-4">
 
-                      <input className="form-control mr-sm-2" type="text" placeholder="Search for a Product!" aria-label="Search" />
+                      <input className="form-control mr-sm-2" type="text" placeholder="Search for a Product!" aria-label="Search" value={this.state.searchTerms}
+                onChange={this.handleInputChange}
+                name="searchTerms" />
 
-                      <button className="btn-rounded btn-sm" type="submit">Find Product</button>
+                      <button className="btn-rounded btn-sm" disabled={!(this.state.searchTerms)}
+                onClick={this.doProductSearch}>Find Product</button>
 
                     </form>
                   </div>
